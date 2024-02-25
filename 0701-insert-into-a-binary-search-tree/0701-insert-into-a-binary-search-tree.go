6e323663a68e536fab091060308e45ce113cc9a9
swillbill <sarahswilley88@gmail.com>
2024-02-25 10:29:51 -0500

    Time: 23 ms (70.70%), Space: 7 MB (84.19%) - LeetHub

---
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.go
@@ -0,0 +1,34 @@
+/*
+PSUEDO
+base case
+if value > root value 
+call insert function on root.right value and asssign to right node 
+else less than case 
+return root
+*/
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+    
+    // base case
+    if root == nil {
+        return &TreeNode{Val: val}
+    }
+
+    if val > root.Val {
+        root.Right = insertIntoBST(root.Right, val)
+    }
+    if val < root.Val {
+        root.Left = insertIntoBST(root.Left, val)
+    }
+
+return root
+}
